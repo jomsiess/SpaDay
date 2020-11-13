282e480e592b0ff88a33fd3492ed8f2472118cf1
jomsiess <jomsiess@gmail.com>
2020-11-12 21:16:11 -0600

    Working on studio...

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Controllers
+{
+    public class UserController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View(); 
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password; 
+        }
+
+    }
+}
