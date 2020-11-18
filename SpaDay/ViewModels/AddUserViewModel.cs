16d49327d9a491d47ac62f84e5d4a0349b2978c6
jomsiess <jomsiess@gmail.com>
2020-11-17 20:36:47 -0600

    * AddUserViewModel.cs:
    * UserController.cs: Finished studio!

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 15 characters.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please verify password")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 20 characters.")]
+        public string VerifyPassword;
+        internal object username;
+    }
+}
